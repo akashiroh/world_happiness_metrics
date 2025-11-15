dae6533a1643319fde7892462ee25e64ee635b46
public andrew <publicandrew@MacBookPro.home>
2025-11-14 21:01:03 -0800

    added histogram plots to explore data

--- exploration.py
+++ exploration.py
@@ -4,7 +4,31 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 def plot_hists():
-    """explore the dataset for trends."""
+    """plot histograms."""
+
+    DATA_DIR = Path("data")
+
+    # load data
+    data_path = DATA_DIR / "merged.csv"
+    df = pd.read_csv(data_path)
+
+    metric = "ladder_score"
+
+    plt.figure(figsize=(12,6))
+
+    plt.hist(
+        df[metric]
+    )
+    plt.title(f'{" ".join(map(str.capitalize, metric.split("_")))} 2023')
+    plt.xlabel(metric)
+    plt.ylabel("count")
+    plt.savefig(f"{metric}_hist.png")
+    print("Saved figure:", f"{metric}_hist.png")
+
+
+
+def plot_map():
+    """plot map."""
 
     DATA_DIR = Path("data")
 
